4cd3f993656957cb9cb2ff78aa8f6c8f7bfc6b7d
Krewca <krewca@seznam.cz>
2020-02-17 13:21:44 +0100

    Pushed Hello World (test)

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HelloWorld
+{
+    void PrintHelloWorld()
+    {
+        print("Hello, world!");
+    }
+}
+
