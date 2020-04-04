0fb4f0739d09bdf7d28c7f8134673dedd14a99eb
Marteni <36880529+Marteni@users.noreply.github.com>
2020-04-04 11:43:04 +0200

    Added basic DBContext

--- MoviesApp.DAL/MoviesAppDbContext.cs
+++ MoviesApp.DAL/MoviesAppDbContext.cs
@@ -2,11 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MoviesApp.DAL.Entities;
 
 namespace MoviesApp.DAL
 {
-    class MoviesAppDbContext: DbContext
+    public  class MoviesAppDbContext: DbContext
     {
+        public DbSet<MovieEntity> Movies { get; set; }
+        public DbSet<RatingEntity> Ratings { get; set; }
+        public DbSet<PersonEntity> People { get; set; }
+        public DbSet<MoviesPersonActorEntity> Actors { get; set; }
+        public DbSet<MoviesPersonDirectorEntity> Directors { get; set; }
     }
 }
  
