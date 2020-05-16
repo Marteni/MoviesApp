a2f589ad664f633d2bf1ab0f7240772c2607ccaa
David <david.spavor@gmail.com>
2020-05-16 14:42:53 +0200

    Added watermark for text for search textbox, some changes in dbcontext factory.

--- MoviesApp.DAL/Factories/DbContextSqlFactory.cs
+++ MoviesApp.DAL/Factories/DbContextSqlFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -8,13 +9,6 @@ namespace MoviesApp.DAL.Factories
     {
         public MoviesAppDbContext CreateAppDbContext()
         {
-
-           // var configuration = new ConfigurationBuilder()
-           //.SetBasePath(Directory.GetCurrentDirectory())
-           //.AddJsonFile("appsettings.json")
-           //.Build();
-           // var connectionString = configuration.GetValue<string>("connectionString");
-
 
             var optionsBuilder = new DbContextOptionsBuilder<MoviesAppDbContext>();
             optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog = MoviesAppDb;MultipleActiveResultSets = True;Integrated Security = True;");
